9c87a51f5efd1f276d68000bdb0bad0f3b16e12f
George2901 <32207537+George2901@users.noreply.github.com>
2023-03-07 07:33:14 -0800

    Update sort.py

--- sort.py
+++ sort.py
@@ -1,14 +1,13 @@
 
-res = 'texts/results.txt'
-btc = 'texts/btc.txt'
 
-target = res
+target = 'texts/results.txt'
 # target = btc
 
 def main():
     ll:set = None
     with open(target,"r", encoding="utf8") as f:
         ll = set(f.read().splitlines())
+        
 
     with open(target,"w", encoding="utf8") as f:
         [f.write(i + '\n') for i in ll]
